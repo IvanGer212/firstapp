8fa668a161fe0c2157b33f23b84dc4f458ff7389
IvanGer212 <Comradeivan88@gmail.com>
2021-08-08 11:59:26 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/app/FirstServlet.java
@@ -0,0 +1,15 @@
+package com.geekbrains.app;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().printf("<html><body><h1>New GET Request</h1></body></html>");
+    }
+}
