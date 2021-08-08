833d219f2fc92a703f927a95ce844ced23c26ebd
IvanGer212 <Comradeivan88@gmail.com>
2021-08-08 12:48:12 +0300

    change method for send product on screen with stream

--- src/main/java/com/geekbrains/app/FirstServlet.java
+++ src/main/java/com/geekbrains/app/FirstServlet.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class FirstServlet extends HttpServlet {
     private static Logger logger = LoggerFactory.getLogger(FirstServlet.class);
@@ -21,14 +22,19 @@ public class FirstServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         logger.info("Log: GET");
         ArrayList<Product> products = new ArrayList<>();
-        Random random = new Random();
-        resp.getWriter().printf("<html><body>");
+        final Random random = new Random();
+        Stream<Product> stream = products.stream();
         for (int i = 1; i <=10 ; i++) {
             products.add(new Product(i,"Product "+i, random.nextDouble()*i*1000));
         }
-        for (int i = 0; i <10 ; i++) {
-            resp.getWriter().printf("<h1>"+products.get(i).toString()+"</h1>");
-        }
+        resp.getWriter().printf("<html><body>");
+        stream.forEach(p-> {
+            try {
+                resp.getWriter().printf("<h1>"+p.toString()+"</h1>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
         resp.getWriter().printf("</body></html>");
     }
 }
